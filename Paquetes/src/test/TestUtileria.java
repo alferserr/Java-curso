202ccfc741976eb09f5851824c3864a744c803fb
alferserr <78552488+alferserr@users.noreply.github.com>
2022-08-11 18:36:16 +0200

    Add files via upload

---
+++ Paquetes/src/test/TestUtileria.java
@@ -0,0 +1,10 @@
+package test;
+//import paquetes.proyecto.Utileria; --> Recomendado
+//import static paquetes.proyecto.Utileria.imprimir;
+public class TestUtileria {
+    public static void main(String[] args) {
+        //Utileria.imprimir("Hola"); --> Recomendado
+        //imprimir("Saludos");
+        paquetes.proyecto.Utileria.imprimir("Prueba");
+    }
+}
